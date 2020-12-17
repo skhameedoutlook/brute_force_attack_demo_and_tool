57045fc4f65a4e4cae907ce86d3933b222e12b66
Shaik Hameed <skhameed@outlook.com>
2020-12-17 16:24:07 +0530

    Cleaned DB file.

--- db_for_fis_attack_demo_tool.sql
+++ db_for_fis_attack_demo_tool.sql
@@ -1,47 +1,20 @@
--- phpMyAdmin SQL Dump
--- version 5.0.3
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Dec 17, 2020 at 11:48 AM
--- Server version: 10.4.14-MariaDB
--- PHP Version: 7.2.34
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `test`
---
 CREATE DATABASE IF NOT EXISTS `test` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
 USE `test`;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `attack_demo_users_1`
---
 
 CREATE TABLE `attack_demo_users_1` (
   `user` varchar(255) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `attack_demo_users_1`
---
+
 
 INSERT INTO `attack_demo_users_1` (`user`, `password`) VALUES
 ('use1', 'cpa');
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
